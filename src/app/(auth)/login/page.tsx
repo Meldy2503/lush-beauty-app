750f379744f03e5acc8bf2d6c48332ab618693ce
Emelder Okafor <meldy@mac.home>
2025-06-11 22:46:50 +0200

    feat: added the sign page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,88 +1,25 @@
 "use client";
 
 import Button from "@/components/ui/button";
-import Logo from "@/components/ui/logo";
-import { Box, Flex, Stack } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
-import bgImage from "../../../assets/images/contact-bg.webp";
+import { Stack } from "@chakra-ui/react";
 import { InputElement } from "../../../components/ui/input-element";
+import AuthWrapper from "@/components/auth/auth-wrapper";
 
 const Login = () => {
-  const router = useRouter();
-
   return (
-    <Flex
-      backgroundImage={`linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.8)), url(${bgImage.src})`}
-      backgroundSize="cover"
-      backgroundPosition="center"
-      height="100vh"
-      width="100vw"
-      position="fixed"
-      top={"0"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      flexDir={"column"}
-    >
-      <Box
-        w={{ base: "95%", md: "80%" }}
-        bg="gray.250"
-        p="2rem"
-        flexDir={"column"}
-      >
-        <Button
-          onClick={() => router.back()}
-          bg="transparent"
-          color="black"
-          px={"0"}
-          hover="transparent"
-        >
-          ← Go Back
-        </Button>
-        <Flex
-          justifyContent={"center"}
-          alignItems={"center"}
-          flexDir={"column"}
-          mb="5rem"
-        >
-          <Logo />
-          <Flex
-            gap="2rem"
-            w={{ base: "100%", md: "80%", xl: "50%" }}
-            direction={"column"}
-            h="100%"
-            mt="4rem"
-            overflow={"auto"}
-          >
-            <form>
-              <Stack gap="2rem">
-                <InputElement label="Full name" placeholder="Peter Smith" />
-                <InputElement
-                  label="Email address"
-                  placeholder="peter@gmail.com"
-                  type="email"
-                />
-                <InputElement
-                  label="Phone number"
-                  placeholder="+447056835551"
-                  type="number"
-                />
-                <Flex gap="2rem" flexDir={{ base: "column", md: "row" }}>
-                  <InputElement label="Country" placeholder="Peter Smith" />
-                  <InputElement label="State" placeholder="Peter Smith" />
-                </Flex>
-                <InputElement label="Address" placeholder="2 Beverley street" />
-                <InputElement
-                  type="password"
-                  label="Password"
-                  placeholder="*****"
-                />
-                <Button w="full">Sign in</Button>
-              </Stack>
-            </form>{" "}
-          </Flex>
-        </Flex>
-      </Box>
-    </Flex>
+    <AuthWrapper authType="login">
+      <form>
+        <Stack gap="3rem">
+          <InputElement
+            label="Email address"
+            placeholder="peter@gmail.com"
+            type="email"
+          />
+          <InputElement type="password" label="Password" placeholder="*****" />
+          <Button w="full">Sign in</Button>
+        </Stack>
+      </form>
+    </AuthWrapper>
   );
 };
 
---
+++ src/components/auth/auth-wrapper.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import Button from "@/components/ui/button";
+import Logo from "@/components/ui/logo";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import bgImage from "../../assets/images/contact-bg.webp";
+
+interface AuthWrapperProps {
+  children: React.ReactNode;
+  authType: keyof typeof authTypes;
+}
+
+const authTypes = {
+  signup: "Create Your Account",
+  login: "Login to Your Account",
+};
+
+const AuthWrapper = ({ children, authType }: AuthWrapperProps) => {
+  const router = useRouter();
+
+  return (
+    <Flex
+      backgroundImage={`linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.8)), url(${bgImage.src})`}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      height="100vh"
+      width="100vw"
+      position="fixed"
+      top={"0"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDir={"column"}
+    >
+      <Box
+        w={{ base: "95%", md: "80%" }}
+        bg="gray.250"
+        p="2rem"
+        flexDir={"column"}
+      >
+        <Button
+          onClick={() => router.back()}
+          bg="transparent"
+          color="black"
+          px={"0"}
+          hover="transparent"
+        >
+          ← Go Back
+        </Button>
+        <Flex
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexDir={"column"}
+          mb="5rem"
+        >
+          <Logo />
+          <Heading
+            as="h2"
+            fontSize={{ base: "1.7rem", md: "2rem" }}
+            lineHeight={1.3}
+            textAlign="center"
+            mt={8}
+          >
+            {authTypes[authType]}
+          </Heading>
+          <Flex
+            gap="2rem"
+            w={{ base: "100%", md: "80%", xl: "50%" }}
+            direction={"column"}
+            h="100%"
+            mt="4rem"
+            overflow={"auto"}
+          >
+            {children}
+          </Flex>
+          <Text textAlign="center" mt={"3rem"}>
+            {authType === "signup"
+              ? "Already have an account?"
+              : "Don’t have an account? "}{" "}
+            <Link
+              href={authType === "signup" ? "/login" : "/sign-up"}
+              style={{ fontWeight: "bold", color: "#A9762A" }}
+            >
+              {authType === "signup" ? "Login" : "Sign up"}
+            </Link>
+          </Text>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
+
+export default AuthWrapper;
